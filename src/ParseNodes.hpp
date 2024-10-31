2cbc2baa9438ff839d305b6e657da3b3f452b7b9
Sky1sBloo <ultimate8gaming@gmail.com>
2024-10-31 17:11:36 +0800

    Added expr node variant

--- src/ParseNodes.hpp
+++ src/ParseNodes.hpp
@@ -1,14 +1,30 @@
 #pragma once
 #include <string>
+#include <variant>
 
 class LiteralExpr {
+public:
     LiteralExpr(const std::string& newValue);
+
 private:
     const std::string value;
 };
 
 class VariableExpr {
+public:
     VariableExpr(const std::string& id);
+
 private:
     const std::string id;
+};
+
+class ExprNode {
+public:
+    auto& getType()
+    {
+        return type;
+    }
+
+private:
+    std::variant<LiteralExpr, VariableExpr> type;
 };
