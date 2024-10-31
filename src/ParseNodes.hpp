82c0f76a2e5d6893918238c0d4c6476db18e2b20
Sky1sBloo <ultimate8gaming@gmail.com>
2024-10-31 16:41:53 +0800

    Added literal and variable expr

--- src/ParseNodes.hpp
+++ src/ParseNodes.hpp
@@ -1,7 +1,14 @@
 #pragma once
+#include <string>
 
-class Expr {
-public:
-    ~Expr() = default;
+class LiteralExpr {
+    LiteralExpr(const std::string& newValue);
 private:
+    const std::string value;
+};
+
+class VariableExpr {
+    VariableExpr(const std::string& id);
+private:
+    const std::string id;
 };
