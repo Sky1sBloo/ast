727c861257a2dd46af0154fabc39ad9bbbfa43f2
Sky1sBloo <ultimate8gaming@gmail.com>
2024-10-31 17:26:53 +0800

    Added getConditionFunction

--- src/BinaryConditionalExpr.cpp
+++ src/BinaryConditionalExpr.cpp
@@ -1,6 +1,25 @@
 #include "ParseNodes.hpp"
 
-BinaryConditionExpr::BinaryConditionExpr(Type conditionType, ExprNodePtr newExprA, ExprNodePtr newExprB)
+BinaryConditionExpr::BinaryConditionExpr(Types conditionType, ExprNodePtr newExprA, ExprNodePtr newExprB)
+    : type(conditionType)
+    , exprA(std::move(newExprA))
+    , exprB(std::move(newExprB))
 {
+}
 
+bool BinaryConditionExpr::getCondition()
+{
+    switch (type) {
+    case Types::EQUALS:
+        break;
+    case Types::GREATER:
+        break;
+    case Types::LESS:
+        break;
+    case Types::GREATER_EQUAL:
+        break;
+    case Types::LESS_EQUAL:
+        break;
+    }
+    return false;
 }
