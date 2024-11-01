a88a760876e5a548a0542f80c5ce3d6aac518729
Sky1sBloo <ultimate8gaming@gmail.com>
2024-11-01 15:57:01 +0800

    Added performAction on tests

--- tests/ParseNodesTests.cpp
+++ tests/ParseNodesTests.cpp
@@ -9,7 +9,28 @@ TEST(PARSE_NOTES_TEST, INITIALIZATION_EXPR_TEST)
     const std::string expectedValue = "5";
 
     InitializationExpr initExpr(std::make_unique<LiteralExpr>(DataType::INT, expectedValue), memory);
+    initExpr.performAction();
     std::string retrievedValue = memory->retrieve(1);
 
-    EXPECT_EQ(expectedValue, retrievedValue);
+    EXPECT_EQ(retrievedValue, expectedValue);
+}
+
+TEST(PARSE_NOTES_TEST, ASSIGNMENT_EXPR_TEST)
+{
+    std::shared_ptr<ProgramMemory> memory;
+
+    const std::string initialValue = "3";
+    const std::string expectedValue = "6";
+
+    InitializationExpr initExpr(std::make_unique<LiteralExpr>(DataType::INT, initialValue), memory);
+    initExpr.performAction();
+    std::string retrievedValue = memory->retrieve(1);
+
+    EXPECT_EQ(retrievedValue, initialValue);
+
+    AssignExpr assignExpr(1, std::make_unique<LiteralExpr>(DataType::INT, expectedValue), memory);
+    assignExpr.performAction();
+    retrievedValue = memory->retrieve(1);
+
+    EXPECT_EQ(retrievedValue, expectedValue);
 }
