a39833f9c3c6f002d07d934c7e99d8e866a121ab
Sky1sBloo <ultimate8gaming@gmail.com>
2024-11-01 16:14:05 +0800

    Initialized shared ptr memory

--- tests/ParseNodesTests.cpp
+++ tests/ParseNodesTests.cpp
@@ -5,32 +5,32 @@
 
 TEST(PARSE_NOTES_TEST, INITIALIZATION_EXPR_TEST)
 {
-    std::shared_ptr<ProgramMemory> memory;
+    std::shared_ptr<ProgramMemory> memory = std::make_shared<ProgramMemory>();
     const std::string expectedValue = "5";
 
     InitializationExpr initExpr(std::make_unique<LiteralExpr>(DataType::INT, expectedValue), memory);
     initExpr.performAction();
-    std::string retrievedValue = memory->retrieve(1);
+    std::string retrievedValue = memory->retrieve(0);
 
     EXPECT_EQ(retrievedValue, expectedValue);
 }
 
 TEST(PARSE_NOTES_TEST, ASSIGNMENT_EXPR_TEST)
 {
-    std::shared_ptr<ProgramMemory> memory;
+    std::shared_ptr<ProgramMemory> memory = std::make_shared<ProgramMemory>();
 
     const std::string initialValue = "3";
     const std::string expectedValue = "6";
 
     InitializationExpr initExpr(std::make_unique<LiteralExpr>(DataType::INT, initialValue), memory);
     initExpr.performAction();
-    std::string retrievedValue = memory->retrieve(1);
+    std::string retrievedValue = memory->retrieve(0);
 
     EXPECT_EQ(retrievedValue, initialValue);
 
     AssignExpr assignExpr(1, std::make_unique<LiteralExpr>(DataType::INT, expectedValue), memory);
     assignExpr.performAction();
-    retrievedValue = memory->retrieve(1);
+    retrievedValue = memory->retrieve(0);
 
     EXPECT_EQ(retrievedValue, expectedValue);
 }
