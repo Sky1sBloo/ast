c097f05f6d26e29113426f446c9dda42e9de7b04
Sky1sBloo <ultimate8gaming@gmail.com>
2024-11-02 20:47:06 +0800

    Added clang format

--- src/ProgramMemory.cpp
+++ src/ProgramMemory.cpp
@@ -1,18 +1,17 @@
 #include "ProgramMemory.hpp"
 #include <stdexcept>
 
-ProgramMemory::ProgramMemory() :
-    _highestPtr(0)
+ProgramMemory::ProgramMemory()
+    : _highestPtr(0)
 {
 }
 
 int ProgramMemory::allocate(const std::string& value)
 {
-    _memory.insert({_highestPtr, value});
+    _memory.insert({ _highestPtr, value });
     _highestPtr++;
     return _highestPtr - 1;
 }
-
 
 const std::string& ProgramMemory::retrieve(int address)
 {
@@ -21,7 +20,6 @@ const std::string& ProgramMemory::retrieve(int address)
     }
     throw std::out_of_range("ProgramMemory at retrieve: Invalid address access");
 }
-
 
 void ProgramMemory::set(int address, const std::string& value)
 {
--- src/ProgramMemory.hpp
+++ src/ProgramMemory.hpp
@@ -1,33 +1,33 @@
 #pragma once
-#include <unordered_map>
 #include <string>
+#include <unordered_map>
 
 // Class handling program memory such as variables
-class ProgramMemory 
-{
+class ProgramMemory {
 public:
     ProgramMemory();
     /**
      * Allocates a value to the memory
      *
      * @return The address of the memory
-    */
+     */
     int allocate(const std::string& value);
 
     /**
      * Retrieves the value of address
      *
      * Throws std::out_of_range if address is invalid
-    */
+     */
     const std::string& retrieve(int address);
 
     /**
      * Sets the value on the adddress pointed
      *
      * Throws std::out_of_range if address is invalid
-    */
+     */
     void set(int address, const std::string& value);
+
 private:
-    int _highestPtr;  // To determine free memory
+    int _highestPtr; // To determine free memory
     std::unordered_map<int, std::string> _memory;
 };
