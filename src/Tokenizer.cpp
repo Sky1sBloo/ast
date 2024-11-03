2525b701e1286749122848f046d32753543cb767
Sky1sBloo <ultimate8gaming@gmail.com>
2024-11-03 18:36:45 +0800

    Added type determination

--- src/Tokenizer.cpp
+++ src/Tokenizer.cpp
@@ -1,15 +1,79 @@
 #include "Tokenizer.hpp"
+#include <algorithm>
+#include <cctype>
 #include <string>
 
 Tokenizer::Tokenizer(std::stringstream& sourceCode)
 {
     std::string word;
     while (sourceCode >> word) {
+        // To avoid checking terminate character on identifying type
+        bool containsTermination = hasTerminateChar(word);
+        if (containsTermination) {
+            word.pop_back();
+        }
+
+        TokenTypes type = identitfyType(word);
+
+        if (containsTermination) {
+            _tokens.push_back(Token(TokenTypes::STATEMENT_TERMINATE, ";"));
+        }
     }
 }
 
-
-TokenTypes identitfyType(const std::string& word)
+bool Tokenizer::hasTerminateChar(const std::string& word) const
 {
+    return (word.back() == _terminationChar);
+}
 
+TokenTypes Tokenizer::identitfyType(const std::string& word)
+{
+    if (isKeyword(word)) {
+        return TokenTypes::KEYWORD;
+    }
+    if (isOperation(word)) {
+        return TokenTypes::OPERATOR;
+    }
+    if (isAssignment(word)) {
+        return TokenTypes::ASSIGN;
+    }
+
+    return TokenTypes::IDENTIFIER;
+};
+
+bool Tokenizer::isKeyword(const std::string& word) const
+{
+    return std::ranges::find(_keywordRuleset, word) != _keywordRuleset.end();
+}
+
+bool Tokenizer::isOperation(const std::string& word) const
+{
+    return std::ranges::find(_operationRuleset, word) != _operationRuleset.end();
+}
+
+bool Tokenizer::isAssignment(const std::string& word) const
+{
+    return (word == _assignmentRuleset);
+}
+
+bool Tokenizer::isLiteral(const std::string& word) const
+{
+    // Check if numerical
+    bool isNumerical = std::ranges::all_of(word, [](char c) { return (std::isdigit(c) || c == '.'); });
+    if (isNumerical) {
+        return true;
+    }
+
+    // Check if string
+    bool isString = word.front() == word.back() && std::ranges::find(_stringLiteralRuleset, word.front()) != _stringLiteralRuleset.end();
+    if (isString) {
+        return true;
+    }
+
+    bool isBoolean = std::ranges::find(_booleanLiteralRuleset, word) != _booleanLiteralRuleset.end();
+    if (isBoolean) {
+        return true;
+    }
+
+    return false;
 }
--- src/Tokenizer.hpp
+++ src/Tokenizer.hpp
@@ -1,14 +1,17 @@
 #pragma once
 
+#include <array>
 #include <sstream>
 #include <vector>
 
 enum class TokenTypes {
+    INVALID,
     LITERAL,
     KEYWORD,
     IDENTIFIER,
-    VARIABLE_DECLARATION,
-    ASSIGN
+    OPERATOR,
+    ASSIGN,
+    STATEMENT_TERMINATE
 };
 
 struct Token {
@@ -25,8 +28,37 @@ struct Token {
 class Tokenizer {
 public:
     Tokenizer(std::stringstream& sourceCode);
+
 private:
     std::vector<Token> _tokens;
 
     TokenTypes identitfyType(const std::string& word);
+    /**
+     * Checks the edge of string if it has termination character
+     */
+    bool hasTerminateChar(const std::string& word) const;
+
+    // The following functions checks by ruleset
+    bool isKeyword(const std::string& word) const;
+    bool isOperation(const std::string& word) const;
+    bool isAssignment(const std::string& word) const;
+    bool isLiteral(const std::string& word) const;
+
+private:
+    // RULESETS
+    constexpr static std::array<std::string, 1> _keywordRuleset = {
+        "var"
+    };
+    constexpr static std::array<std::string, 4> _operationRuleset = {
+        "+", "-", "/", "*"
+    };
+    constexpr static std::array<char, 2> _stringLiteralRuleset = {
+        '\'', '\"'
+    };
+    constexpr static std::array<std::string, 4> _booleanLiteralRuleset = {
+        "true", "True", "false", "False"
+    };
+
+    constexpr static std::string _assignmentRuleset = "==";
+    constexpr static char _terminationChar = ';';
 };
