1784717c47c64ffdde215607aa916bb0f9ea4727
Sky1sBloo <ultimate8gaming@gmail.com>
2024-11-04 17:16:07 +0800

    Updated tokenizer test to include strings

--- tests/TokenizerTests.cpp
+++ tests/TokenizerTests.cpp
@@ -8,11 +8,18 @@
 TEST(TOKENIZER_TESTS, READING)
 {
     std::stringstream source;
-    source << "var a = 5; var b = 2.0 ;";
-    const std::array<TokenTypes, 10> expectedTypes = {
+    source << "var a = 5; var b = 2.0 + 5.0;\nvar c = \"fifty\";\n";
+    const std::array<TokenTypes, 17> expectedTypes = {
         TokenTypes::KEYWORD,
         TokenTypes::IDENTIFIER,
         TokenTypes::ASSIGN,
+        TokenTypes::LITERAL,
+        TokenTypes::STATEMENT_TERMINATE,
+        TokenTypes::KEYWORD,
+        TokenTypes::IDENTIFIER,
+        TokenTypes::ASSIGN,
+        TokenTypes::LITERAL,
+        TokenTypes::OPERATOR,
         TokenTypes::LITERAL,
         TokenTypes::STATEMENT_TERMINATE,
         TokenTypes::KEYWORD,
