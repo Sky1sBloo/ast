a70ab6542dadf7208c50d88e1bb0dda03a03c5a3
JD Paldeng <68984863+Sky1sBloo@users.noreply.github.com>
2024-11-05 15:17:43 +0800

    Removed unique ptr usage on memory (#6)

--- src/ProgramMemory.cpp
+++ src/ProgramMemory.cpp
@@ -8,14 +8,14 @@ ProgramMemory::ProgramMemory()
 
 int ProgramMemory::allocate(const std::string& value)
 {
-    _memory.insert({ _highestPtr, std::make_unique<MemoryCell>(value) });
+    _memory.emplace(_highestPtr, value);
     _highestPtr++;
     return _highestPtr - 1;
 }
 
 int ProgramMemory::allocate(const MemoryCell& cell)
 {
-    _memory.insert({ _highestPtr, std::make_unique<MemoryCell>(cell) });
+    _memory.emplace(_highestPtr, cell);
     _highestPtr++;
     return _highestPtr - 1;
 }
@@ -23,7 +23,7 @@ int ProgramMemory::allocate(const MemoryCell& cell)
 const MemoryCell& ProgramMemory::retrieve(int address) const
 {
     if (_memory.contains(address)) {
-        return *_memory.at(address);
+        return _memory.at(address);
     }
     throw std::out_of_range("ProgramMemory at retrieve: Invalid address access");
 }
@@ -34,7 +34,7 @@ void ProgramMemory::set(int address, const std::string& value)
         throw std::out_of_range("ProgramMemory set: Invalid address access");
     }
 
-    _memory.at(address)->set(value);
+    _memory.at(address).set(value);
 }
 
 void ProgramMemory::set(int address, const MemoryCell& cell)
@@ -43,5 +43,5 @@ void ProgramMemory::set(int address, const MemoryCell& cell)
         throw std::out_of_range("ProgramMemory set: Invalid address access");
     }
 
-    _memory.at(address) = std::make_unique<MemoryCell>(cell);
+    _memory.at(address) = cell;
 }
