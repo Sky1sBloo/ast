2530bf20ead0083de1bf2c6ea90a20d2e47bd99b
Sky1sBloo <ultimate8gaming@gmail.com>
2024-11-14 18:47:14 +0800

    Fixed tests to add semi colon

--- tests/ParseTreeBuilderTests.cpp
+++ tests/ParseTreeBuilderTests.cpp
@@ -11,7 +11,7 @@ TEST(PARSE_TREE_TESTS, VARIABLE_SAVING)
 {
     std::shared_ptr<ProgramMemory> memory = std::make_shared<ProgramMemory>();
     std::shared_ptr<VariableHandler> variableHandler = std::make_shared<VariableHandler>(memory);
-    std::string source = "a = 5";
+    std::string source = "a = 5;";
     int expectedValue = 5;
 
     variableHandler->allocate("a");
@@ -24,7 +24,25 @@ TEST(PARSE_TREE_TESTS, VARIABLE_SAVING)
     MemoryCell value = variableHandler->getValue("a");
     auto returnedValue = value.getAs<int>();
     if (returnedValue == std::nullopt) {
-        FAIL() << "Returned variable value isn't at expected type";
+        std::string type;
+        switch (value.getType()) {
+        case DataType::INT:
+            type = "int";
+            break;
+        case DataType::BOOL:
+            type = "bool";
+            break;
+        case DataType::FLOAT:
+            type = "float";
+            break;
+        case DataType::STRING:
+            type = "string";
+            break;
+        case DataType::NULL_TYPE:
+            type = "null";
+            break;
+        }
+        FAIL() << "Returned variable value isn't at expected type. Type is: " << type;
     }
 
     EXPECT_EQ(expectedValue, returnedValue);
