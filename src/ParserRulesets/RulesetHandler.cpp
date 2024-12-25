1e71658ea79e815921d3b0ec269f38dfffdf3d5d
Sky1sBloo <ultimate8gaming@gmail.com>
2024-12-25 17:29:50 +0800

    Added ruleset handler

---
+++ src/ParserRulesets/RulesetHandler.cpp
@@ -0,0 +1,23 @@
+#include "RulesetHandler.hpp"
+#include "ParserRulesets/ParserRulesets.hpp"
+#include <stdexcept>
+
+RulesetHandler::RulesetHandler(std::shared_ptr<VariableHandler> handler)
+    : _rulesets(3)
+{
+    _rulesets.push_back(std::make_unique<VariableInitializationRuleset>(handler));
+    _rulesets.push_back(std::make_unique<VariableAssignmentRuleset>(handler));
+    _rulesets.push_back(std::make_unique<VariableInitializationAndAssignmentRuleset>(handler));
+}
+
+std::unique_ptr<Expr> RulesetHandler::getExpression(const std::vector<Token>& statement)
+{
+    for (const auto& ruleset : _rulesets) {
+        std::unique_ptr<Expr> expr = ruleset->createExpr(statement);
+        if (expr != nullptr) {
+            return std::move(expr);
+        }
+    }
+
+    throw std::invalid_argument("Statement ruleset is unkown");
+}
---
+++ src/ParserRulesets/RulesetHandler.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "BaseParseNodes.hpp"
+#include "ParserRulesets/ParserRulesets.hpp"
+#include "Token.hpp"
+#include "VariableHandler.hpp"
+#include <memory>
+
+/**
+ * Class for handling rulesets
+ */
+class RulesetHandler {
+public:
+    RulesetHandler(std::shared_ptr<VariableHandler> handler);
+
+    /**
+     * Checks all rulesets and returns an expression
+     *
+     * @throws invalid_argument if it doesnt match any ruleset
+    */
+    std::unique_ptr<Expr> getExpression(const std::vector<Token>& statement);
+
+private:
+    std::vector<std::unique_ptr<ParserRuleset>> _rulesets;
+};
